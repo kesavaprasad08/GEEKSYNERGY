d5467444cd7a54f9886d448772009164d027fa99
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-23 00:00:01 +0530

    final

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 
 import "./App.css";
-import SignUp from "./pages/SignUp";  
+import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
-import Home from "./pages/Home";  
+import Home from "./pages/Home";
 import CompanyInfo from "./components/CompanyInfo";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showInfo,setShowInfo]=useState(false);
+  const [showInfo, setShowInfo] = useState(false);
 
-  const ShowInfoHandler=()=>{
-    setShowInfo(true)
-  }
-const hideInfoHandler=()=>{
-  setShowInfo(false);
-}
+  const ShowInfoHandler = () => {
+    setShowInfo(true);
+  };
+  const hideInfoHandler = () => {
+    setShowInfo(false);
+  };
 
   const loginHandler = () => {
     setIsLoggedIn(true);
   };
   return (
     <div className="App">
-      {showInfo && <CompanyInfo  onClose={hideInfoHandler}/>}
+      {showInfo && <CompanyInfo onClose={hideInfoHandler} />}
       <Routes>
-        <Route path="/" element={<SignUp onSignUp={loginHandler} onOpen={ShowInfoHandler} />}></Route>
+        <Route
+          path="/"
+          element={<SignUp onSignUp={loginHandler} onOpen={ShowInfoHandler} />}
+        ></Route>
         <Route
           path="/login"
           element={<Login onLogin={loginHandler} onOpen={ShowInfoHandler} />}
         ></Route>
-        {isLoggedIn && <Route path="/home" element={<Home onOpen={ShowInfoHandler} />}></Route>}
+        {isLoggedIn && (
+          <Route
+            path="/home"
+            element={<Home onOpen={ShowInfoHandler} />}
+          ></Route>
+        )}
       </Routes>
     </div>
   );
